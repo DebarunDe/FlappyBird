b48ede2313f499fdd9ca464225c2407f11bba616
DebarunDe <42978582+DebarunDe@users.noreply.github.com>
2022-11-08 16:02:36 -0600

    Worked on Bird and initialized in toplevel

--- Bird.sv
+++ Bird.sv
@@ -1,7 +1,92 @@
 module Bird (
-
+input Reset, frame_clk,
+input [7:0] keycode,
+output [9:0]  BirdX, BirdY, BirdS
 );
 
+    logic [9:0] Bird_X_Pos, Bird_X_Motion, Bird_Y_Pos, Bird_Y_Motion, Bird_Size, Bird_Dy_Motion, Gravity;
+	 
+    parameter [9:0] Bird_X_Center= 160;  // Left-Center position on the X axis
+    parameter [9:0] Bird_Y_Center= 240;  // Center position on the Y axis
+    parameter [9:0] Bird_X_Min= 0;       // Leftmost point on the X axis
+    parameter [9:0] Bird_X_Max= 639;     // Rightmost point on the X axis
+    parameter [9:0] Bird_Y_Min= 0;       // Topmost point on the Y axis
+    parameter [9:0] Bird_Y_Max= 479;     // Bottommost point on the Y axis
+    parameter [9:0] Bird_X_Step= 1;      // Step size on the X axis
+    parameter [9:0] Bird_Y_Step= 1;      // Step size on the Y axis
 
+	 assign Ball_Size = 4;  // assigns the value 4 as a 10-digit binary number, ie "0000000100"
+	 //assign Bird_Dy_Motion = 0; // set motion of y direction of bird to be non-constant
+	 assign Gravity = 1; //assign gravity like constant to make the bird feel like it has weight
+	 
+	 always_ff @ (posedge Reset or posedge frame_clk )
+    begin: Move_Bird
+        if (Reset)  // Asynchronous Reset
+        begin 
+            Bird_Y_Motion <= 10'd0; //Ball_Y_Step;
+				Bird_X_Motion <= 10'd0; //Ball_X_Step;
+				Bird_Y_Pos <= Bird_Y_Center;
+				Bird_X_Pos <= Bird_X_Center;
+        end
+		  
+        else 
+        begin 
+				 if ( (Bird_Y_Pos + Bird_Size) >= Bird_Y_Max )  ;// Bird is at the bottom edge, end game
+					  //End game condition here 
+					  
+				 else if ( (Bird_Y_Pos - Bird_Size) <= Bird_Y_Min )  ;// Bird is at the top edge, end game
+					  //End game condition here
+					  
+				  // Bird should never reach right or left edge of the screen, no checks needed
+					  
+				 else
+				 begin
+					  Bird_Dy_Motion <= Bird_Dy_Motion + Gravity; // add gravity to bird to make it move down eventually
+					  Bird_Y_Motion <= Bird_Y_Motion - Bird_Dy_Motion;  // Bird is somewhere in the middle, don't bounce, just keep moving
+				 case (keycode)
+					8'h44 : begin //Spacebar hit 
+
+								Bird_X_Motion <= 0; 
+								Bird_Y_Motion<= 2;
+							  end
+					        
+					/* 8'h07 : begin
+								
+					        Ball_X_Motion <= 1;//D
+							  Ball_Y_Motion <= 0;
+							  end
+
+							  
+					8'h16 : begin
+
+					        Ball_Y_Motion <= 1;//S
+							  Ball_X_Motion <= 0;
+							 end
+							  
+					8'h1A : begin
+					        Ball_Y_Motion <= -1;//W
+							  Ball_X_Motion <= 0;
+							 end	  
+					default: ; */ //Can incorporate keycodes to reset/start/move screens
+			   endcase
+				
+				end
+				
+				
+				 
+				 Bird_Y_Pos <= (Bird_Y_Pos + Bird_Y_Motion);  // Update ball position
+				 Bird_X_Pos <= (Bird_X_Pos + Bird_X_Motion);
+			
+			
+			
+		end  
+    end
+       
+    assign BirdX = Bird_X_Pos;
+   
+    assign BirdY = Bird_Y_Pos;
+   
+    assign BirdS = Bird_Size;
+    
 
 endmodule
--- FlappyBird.sv
+++ FlappyBird.sv
@@ -1,8 +1,93 @@
 module FlappyBird (
 
+      input     MAX10_CLK1_50, 
+      input    [ 1: 0]   KEY,
+      output   [ 9: 0]   LEDR,
+      output             DRAM_CLK,
+      output             DRAM_CKE,
+      output   [12: 0]   DRAM_ADDR,
+      output   [ 1: 0]   DRAM_BA,
+      inout    [15: 0]   DRAM_DQ,
+      output             DRAM_LDQM,
+      output             DRAM_UDQM,
+      output             DRAM_CS_N,
+      output             DRAM_WE_N,
+      output             DRAM_CAS_N,
+      output             DRAM_RAS_N,
+      output             VGA_HS,
+      output             VGA_VS,
+      output   [ 3: 0]   VGA_R,
+      output   [ 3: 0]   VGA_G,
+      output   [ 3: 0]   VGA_B,
+      inout    [15: 0]   ARDUINO_IO,
+      inout              ARDUINO_RESET_N 
+
 
 );
 
+//Local Logic declaration (SoC)
+logic SPI0_CS_N, SPI0_SCLK, SPI0_MISO, SPI0_MOSI, USB_GPX, USB_IRQ, USB_RST;
+logic [1:0] signs, hundreds;
+logic [3:0] hex_num_4, hex_num_3, hex_num_1, hex_num_0; //4 bit input hex digits
+
+
+//Local Logic declaration (Bird)
+logic Reset_h;
+logic [7:0] keycode;
+logic [9:0] birdx, birdy, birdsize;
+
+
+
+FlappyBird_soc u0 (
+		.clk_clk                           (MAX10_CLK1_50),  //clk.clk
+		.reset_reset_n                     (1'b1),           //reset.reset_n
+		.altpll_0_locked_conduit_export    (),               //altpll_0_locked_conduit.export
+		.altpll_0_phasedone_conduit_export (),               //altpll_0_phasedone_conduit.export
+		.altpll_0_areset_conduit_export    (),               //altpll_0_areset_conduit.export
+		.key_external_connection_export    (KEY),            //key_external_connection.export
+
+		//SDRAM
+		.sdram_clk_clk(DRAM_CLK),                            //clk_sdram.clk
+		.sdram_wire_addr(DRAM_ADDR),                         //sdram_wire.addr
+		.sdram_wire_ba(DRAM_BA),                             //.ba
+		.sdram_wire_cas_n(DRAM_CAS_N),                       //.cas_n
+		.sdram_wire_cke(DRAM_CKE),                           //.cke
+		.sdram_wire_cs_n(DRAM_CS_N),                         //.cs_n
+		.sdram_wire_dq(DRAM_DQ),                             //.dq
+		.sdram_wire_dqm({DRAM_UDQM,DRAM_LDQM}),              //.dqm
+		.sdram_wire_ras_n(DRAM_RAS_N),                       //.ras_n
+		.sdram_wire_we_n(DRAM_WE_N),                         //.we_n
+
+		//USB SPI	
+		.spi0_SS_n(SPI0_CS_N),
+		.spi0_MOSI(SPI0_MOSI),
+		.spi0_MISO(SPI0_MISO),
+		.spi0_SCLK(SPI0_SCLK),
+		
+		//USB GPIO
+		.usb_rst_export(USB_RST),
+		.usb_irq_export(USB_IRQ),
+		.usb_gpx_export(USB_GPX),
+		
+		//LEDs and HEX
+		.hex_digits_export({hex_num_4, hex_num_3, hex_num_1, hex_num_0}),
+		.leds_export({hundreds, signs, LEDR}),
+		.keycode_export(keycode)
+		
+	 );
+
+Bird u1 (
+		.Reset(Reset_h),
+		.frame_clk(VGA_VS), //VGA_Clk
+		.keycode(keycode),
+		.BirdX(birdx),
+		.BirdY(birdy), 
+		.BirdS(birdsize)
+);
+
+
+
+	
 
 
 endmodule 
